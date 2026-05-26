0edc1d447602a06686293cfa28077378c91cb85b
Joseph Eton <5099886+etonjoe@users.noreply.github.com>
2026-05-26 22:57:15 +0100

    Create App component with main layout structure

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import About from './components/About'
+import Projects from './components/Projects'
+import Blog from './components/Blog'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div className="bg-white">
+      <Header />
+      <Hero />
+      <About />
+      <Projects />
+      <Blog />
+      <Contact />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
